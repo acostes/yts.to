85febeda9dbaa2e886b46be18a8633311f8b0e0b
acostes <arnaud.costes@gmail.com>
2016-10-15 11:59:38 +0200

    update some tests

--- tests/index.js
+++ tests/index.js
@@ -2,18 +2,17 @@ var test = require('tape');
 var YTS = require('../');
 
 test('search for movies', function (t) {
-  t.plan(8);
+  t.plan(7);
 
   var yts = new YTS();
   yts.findMovie('Snatch', {with_rt_ratings: true}).then( function(movies) {
     t.equal(movies.status, 'ok', 'Request success');
-    t.equal(movies.data.movies[0].title, 'Snatch.', 'Title matches');
-    t.equal(movies.data.movies[0].id, 10, 'ID matches');
+    t.equal(movies.data.movies[0].title, 'Snatch', 'Title matches');
+    t.equal(movies.data.movies[0].id, 2776, 'ID matches');
     t.ok(movies.data.movies[0].torrents.length, 'Has at least one torrent');
     t.ok(movies.data.movies[0].background_image, 'Has background image');
     t.ok(movies.data.movies[0].medium_cover_image, 'Has cover image');
     t.ok(movies.data.movies[0].rating, 'Has rating');
-    t.ok(movies.data.movies[0].rt_critics_score, 'Has rt score');
   });
 });
 
@@ -21,13 +20,13 @@ test('get details for movie', function (t) {
   t.plan(6);
 
   var yts = new YTS();
-  yts.getDetails(10, {with_cast: true}).then( function(movie) {
+  yts.getDetails(2776, {with_cast: true}).then( function(movie) {
     t.equal(movie.status, 'ok', 'Request success');
-    t.equal(movie.data.title, 'Snatch.', 'Title matches');
-    t.ok(movie.data.torrents.length, 'Has at least one torrent');
-    t.ok(movie.data.description_intro, 'Has description');
-    t.ok(movie.data.rating, 'Has rating');
-    t.ok(movie.data.actors, 'Has actors');
+    t.equal(movie.data.movie.title, 'Snatch', 'Title matches');
+    t.ok(movie.data.movie.torrents.length, 'Has at least one torrent');
+    t.ok(movie.data.movie.description_intro, 'Has description');
+    t.ok(movie.data.movie.rating, 'Has rating');
+    t.ok(movie.data.movie.cast, 'Has actors');
   });
 });
 
@@ -35,9 +34,9 @@ test('get suggestions for movie', function (t) {
   t.plan(2);
 
   var yts = new YTS();
-  yts.getSuggestions(10).then( function(movies) {
+  yts.getSuggestions(2776).then( function(movies) {
     t.equal(movies.status, 'ok', 'Request success');
-    t.ok(movies.data.movie_suggestions, 'Has suggestions');
+    t.ok(movies.data.movies, 'Has suggestions');
   });
 });
 
@@ -45,7 +44,7 @@ test('get comments for movie', function (t) {
   t.plan(3);
 
   var yts = new YTS();
-  yts.getComments(10).then( function(comments) {
+  yts.getComments(2776).then( function(comments) {
     t.equal(comments.status, 'ok', 'Request success');
     t.ok(comments.data.comment_count, 'Has comment count');
     t.ok(comments.data.comments, 'Has comments');
